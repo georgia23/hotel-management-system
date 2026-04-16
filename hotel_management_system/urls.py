0a8c15dbe4709d9c362d13f4c65ce17073e33ac6
Georgia Tusiime B <tusigeorgia.gt@gmail.com>
2026-04-17 00:56:39 +0300

    Updated the urls to be app specific

--- hotel_management_system/urls.py
+++ hotel_management_system/urls.py
@@ -15,16 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rooms.views import rooms, roomsList, base, category, categoryList, editRoom, editCategory
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', roomsList, name="roomsList"),
-    path('rooms', rooms , name="rooms"),
-    path('rooms/<int:room_id>/edit', editRoom, name="editRoom"),
-    path('categoryList', categoryList, name="categoryList"),
-    path('category', category , name="category"),
-    path('categoryList/<int:category_id>/edit', editCategory, name="editCategory"),
-    path('base', base, name="base")
+    path('',include('rooms.urls')),
+
+    # path('', roomsList, name="roomsList"),
+    # path('rooms', rooms , name="rooms"),
+    # path('rooms/<int:room_id>/edit', editRoom, name="editRoom"),
+    # path('categoryList', categoryList, name="categoryList"),
+    # path('category', category , name="category"),
+    # path('categoryList/<int:category_id>/edit', editCategory, name="editCategory"),
+    # path('base', base, name="base")
 ]
